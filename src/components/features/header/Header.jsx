03a34a47d2687e9200e76ce848a440e36fd2283f
SlumpG <shlomo.gangul1@gmail.com>
2021-08-14 19:09:36 +0300

    adding multi language support

--- src/components/features/header/Header.jsx
+++ src/components/features/header/Header.jsx
@@ -8,28 +8,38 @@ import {
   faReact,
 } from "@fortawesome/free-brands-svg-icons";
 import Input from "../input/Input";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
   return (
     <div className="header-body">
       <div className="grid-column-one">
         <div>
-          <h4>אתר הוקים ריאקט גלובלי</h4>
+          <h4>{t("navBarMainTitle")}</h4>
         </div>
         <div>
-          <select id="language" className="select-style">
-            <option value=""></option>
-            <option value="Hebrew">עברית</option>
-            <option value="English">אנגלית</option>
-            <option value="Spanish">ספרדית</option>
+          <select
+            onClick={(e) => changeLanguage(`${e.target.value}`)}
+            id="language"
+            className="select-style"
+          >
+            <option value="">בחר שפה</option>
+            <option value="he">עברית</option>
+            <option value="en">English</option>
+            <option value="Spanish">Spanish</option>
           </select>
           <label htmlFor="language" className="label-language">
-            בחר שפה
+            {t("navBarPickLanguage")}
           </label>
         </div>
       </div>
       <div className="grid-column-two">
-        המדריך המלא לג'וניור
+        {t("navBarTitle")}
         <FontAwesomeIcon icon={faReact} style={{ fontSize: "25px" }} />
       </div>
       <div className="grid-column-three">
